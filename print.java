b0fcc665090b8417977ba16af90e35306116550f
MATHESH M <156519287+Alphamale916@users.noreply.github.com>
2024-04-03 22:27:29 +0530

    First Commit

---
+++ print.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class print {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String name=sc.next();
+        int age=sc.nextInt();
+        System.out.println(name+age);
+    }
+}
